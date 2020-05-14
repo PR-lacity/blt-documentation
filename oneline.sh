62992971046f148eb485b99c2c4ef0b8e0509387
PR-lacity <philip.rees@lacity.org>
2020-05-14 11:30:26 -0700

    update scripts

--- oneline.sh
+++ oneline.sh
@@ -2,7 +2,7 @@
 if [[ $EUID -ne 0 ]]; then
     # Get and install updates
     echo "Checking for updates"
-    sudo apt update -y 
+    sudo apt update
     echo "Installing updates. This may take a bit."
     sudo apt upgrade -y 
     # Add php repository
@@ -12,26 +12,8 @@ if [[ $EUID -ne 0 ]]; then
     echo "Refreshing repositories"
     sudo apt-get update -y 
     # Install PHP, mysql, unzip, and git
-    echo "Installing PHP (1/7)"
-    sudo apt-get install -y php7.2-cli 
-    echo "Installing PHP (2/7)"
-    sudo apt-get install -y php7.2-curl 
-    echo "Installing PHP (3/7)"    
-    sudo apt-get install -y php7.2-xml 
-    echo "Installing PHP (4/7)"    
-    sudo apt-get install -y php7.2-mbstring 
-    echo "Installing PHP (5/7)"    
-    sudo apt-get install -y php7.2-bz2 
-    echo "Installing PHP (6/7)"    
-    sudo apt-get install -y php7.2-gd 
-    echo "Installing PHP (7/7)"    
-    sudo apt-get install -y php7.2-mysql 
-    echo "Installing MySQL"
-    sudo apt-get install -y mysql-client 
-    echo "Installing Unzip"
-    sudo apt-get install -y unzip 
-    echo "Installing git"
-    sudo apt-get install -y git 
+    echo "Installing dependencies"
+    sudo apt-get install -y php7.2-cli php7.2-curl php7.2-xml php7.2-mbstring php7.2-bz2 php7.2-gd php7.2-mysql mysql-client unzip git 
     # Download composer
     echo "Downloadind Composer package manager"
     php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
@@ -41,7 +23,7 @@ if [[ $EUID -ne 0 ]]; then
     # Allow composer to be run globally
     sudo mv composer.phar /usr/local/bin/composer
     # Install drush globally
-    echo "Installing Drush"
+    echo "Installing Drush" 
     composer global require drush/drush
     export PATH="$HOME/.config/composer/vendor/bin:$PATH"
     # Install prestissimo - composer enhancement to allow parallel installations

8f92f9f3dc16858a7ccb6a03ae95accb18ad9449
PR-lacity <philip.rees@lacity.org>
2020-06-05 15:15:32 -0700

    Add version select and optimizations

--- oneline.sh
+++ oneline.sh
@@ -23,15 +23,19 @@ if [[ $EUID -ne 0 ]]; then
     echo "==================================================="
     echo ""
     sudo apt-get update -y 
+    # Get php version to install
+    echo "Enter php version to use. e.g. 7.3"
+    echo "Entering an invalid version number will likely result in errors."
+    read PHPver
     # Install PHP, mysql, unzip, and git
     echo "==================================================="
-    echo "Installing dependencies"
+    echo "Installing dependencies using PHP " $PHPver 
     echo "==================================================="
     echo ""
-    sudo apt-get install -y php7.2-cli php7.2-curl php7.2-xml php7.2-mbstring php7.2-bz2 php7.2-gd php7.2-mysql mysql-client unzip git 
+    sudo apt-get install -y php$PHPver-cli php$PHPver-curl php$PHPver-xml php$PHPver-mbstring php$PHPver-bz2 php$PHPver-gd php$PHPver-mysql mysql-client unzip git 
     # Download composer
     echo "==================================================="
-    echo "Downloadind Composer package manager"
+    echo "Downloading Composer package manager"
     echo "==================================================="
     echo ""
     php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
@@ -43,6 +47,12 @@ if [[ $EUID -ne 0 ]]; then
     php composer-setup.php -y 
     # Allow composer to be run globally
     sudo mv composer.phar /usr/local/bin/composer
+    # Install prestissimo - composer enhancement to allow parallel installations
+    echo "==================================================="
+    echo "Installing Prestissimo"
+    echo "==================================================="
+    echo ""
+    composer global require hirak/prestissimo:^0.3 
     # Install drush globally
     echo "==================================================="
     echo "Installing Drush" 
@@ -50,12 +60,6 @@ if [[ $EUID -ne 0 ]]; then
     echo ""
     composer global require drush/drush
     export PATH="$HOME/.config/composer/vendor/bin:$PATH"
-    # Install prestissimo - composer enhancement to allow parallel installations
-    echo "==================================================="
-    echo "Installing Prestissimo"
-    echo "==================================================="
-    echo ""
-    composer global require hirak/prestissimo:^0.3 
     # Install composer drupal optimizations - improves composer performance for drupal projects
     echo "==================================================="
     echo "Installing Composer Drupal Optimizations"
@@ -63,14 +67,14 @@ if [[ $EUID -ne 0 ]]; then
     echo ""
     composer global require zaporylie/composer-drupal-optimizations:^1.1 
     # Prompt user to enter git email address
-    echo Enter git email
+    echo "Enter git email"
     # Get user input for git email.  
     # Add input validation
     read gitEmail
     # Set git email
     git config --global user.email "$gitEmail"
     # Prompt user to enter git username
-    echo Enter git username
+    echo "Enter git username"
     # Get user input for git username
     read gitName
     # Set git username
@@ -82,7 +86,7 @@ if [[ $EUID -ne 0 ]]; then
     echo ""
     composer clear-cache
     # Set composer timeout
-    export COMPOSER_PROCESS_TIMEOUT=2000
+    # export COMPOSER_PROCESS_TIMEOUT=2000
     # unlink composer setup
     php -r "unlink('composer-setup.php');"
 

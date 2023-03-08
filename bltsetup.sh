0780385632c89582062bedb9c1e176298a919040
Philip R <49498464+PR-lacity@users.noreply.github.com>
2023-03-08 13:18:01 -0800

    Update bltsetup.sh
    Change BLT setup to use blt-launcher (https://github.com/acquia/blt-launcher)
    Remove Drupal Optimizations as they are no longer needed with Composer version 2

--- bltsetup.sh
+++ bltsetup.sh
@@ -64,38 +64,17 @@ if [[ $EUID -ne 0 ]]; then
     composer global require drush/drush
     echo "Setting environment variables for Drush"
     echo 'export PATH="$HOME/.config/composer/vendor/bin:$PATH"' | tee -a ~/.profile ~/.zprofile >/dev/null
-    # Install composer drupal optimizations - improves composer performance for drupal projects
-    echo "==================================================="
-    echo "Installing Composer Drupal Optimizations"
-    echo "==================================================="
-    echo ""
-    composer global require zaporylie/composer-drupal-optimizations:^1.2 
     echo "==================================================="
     echo "Installing Acquia BLT"
     echo "==================================================="
     echo ""
-    echo "Setting environment variables for BLT"
-    echo 'function blt() {
-if [[ ! -z ${AH_SITE_ENVIRONMENT} ]]; then
-    PROJECT_ROOT="/var/www/html/${AH_SITE_GROUP}.${AH_SITE_ENVIRONMENT}"
-elif [ "`git rev-parse --show-cdup 2> /dev/null`" != "" ]; then
-    PROJECT_ROOT=$(git rev-parse --show-cdup)
-else
-    PROJECT_ROOT="."
-fi
-
-if [ -f "$PROJECT_ROOT/vendor/bin/blt" ]; then
-    $PROJECT_ROOT/vendor/bin/blt "$@"
-
-# Check for local BLT.
-elif [ -f "./vendor/bin/blt" ]; then
-    ./vendor/bin/blt "$@"
-
-else
-    echo "You must run this command from within a BLT-generated project."
-    return 1
-fi
-        }' | tee -a ~/.profile ~/.zprofile >/dev/null
+    wget -O blt.phar https://github.com/acquia/blt-launcher/releases/latest/download/blt.phar
+    chmod +x blt.phar
+    sudo mv blt.phar /usr/local/bin/blt
+    echo "==================================================="
+    echo "Setting up Git"
+    echo "==================================================="
+    echo ""
     # Prompt user to enter git email address
     echo "Enter git email"
     # Get user input for git email.  
